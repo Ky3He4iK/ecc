1712d0789aaa55b7c44c261adff41e29117352a5
ky3he4ik <ky3he4ik>
2019-12-18 12:48:04 +0300

    init ui

--- gui/MainWindow.cpp
+++ gui/MainWindow.cpp
@@ -2,35 +2,189 @@
 // Created by ky3he4ik on 12/12/2019.
 //
 
+#include <QRegExpValidator>
 #include "MainWindow.h"
+#include "../curve/Curve_parameters.h"
 
-MainWindow::MainWindow() {
+MainWindow::MainWindow(QWidget *) : ecc(ECC(Curve_parameters::curve_secp256k1())) {
     layout = new QGridLayout();
 
-    curveWidget = new CurveWidget();
-    layout->addWidget(curveWidget, 0, 0);
+    label_a = new QLabel("a:");
+    layout->addWidget(label_a, 0, 0);
+    label_b = new QLabel("b:");
+    layout->addWidget(label_b, 1, 0);
+    label_p = new QLabel("p:");
+    layout->addWidget(label_p, 2, 0);
+    label_base_point = new QLabel("base point:");
+    layout->addWidget(label_base_point, 3, 0);
+    label_order = new QLabel("curve order:");
+    layout->addWidget(label_order, 4, 0);
+    label_private = new QLabel("private key:");
+    layout->addWidget(label_private, 5, 0);
+    label_public = new QLabel("public key:");
+    layout->addWidget(label_public, 6, 0);
+    label_other_public = new QLabel("other's public key:");
+    layout->addWidget(label_other_public, 7, 0);
+    label_shared = new QLabel("shared private key:");
+    layout->addWidget(label_shared, 8, 0);
+    label_clear_text = new QLabel("clear text:");
+    layout->addWidget(label_clear_text, 9, 0);
+    label_cypher = new QLabel("cyphered text:");
+    layout->addWidget(label_cypher, 10, 0);
+    label_base64 = new QLabel("cyphered text:\n(base 64)");
+    layout->addWidget(label_base64, 11, 0);
+    label_sign = new QLabel();
+    layout->addWidget(label_sign, 12, 0);
 
-    keysHolder = new KeysHolder();
-    layout->addWidget(keysHolder, 1, 0);
+    edit_a = new QLineEdit();
+    if (is_for_point[0])
+        edit_a->setValidator(new QRegExpValidator(QRegExp("0[2-4].*")));
+    layout->addWidget(edit_a, 0, 1);
+    edit_b = new QLineEdit();
+    if (is_for_point[1])
+        edit_b->setValidator(new QRegExpValidator(QRegExp("0[2-4].*")));
+    layout->addWidget(edit_b, 1, 1);
+    edit_p = new QLineEdit();
+    if (is_for_point[2])
+        edit_p->setValidator(new QRegExpValidator(QRegExp("0[2-4].*")));
+    layout->addWidget(edit_p, 2, 1);
+    edit_base_point = new QLineEdit();
+    if (is_for_point[3])
+        edit_base_point->setValidator(new QRegExpValidator(QRegExp("0[2-4].*")));
+    layout->addWidget(edit_base_point, 3, 1);
+    edit_order = new QLineEdit();
+    if (is_for_point[4])
+        edit_order->setValidator(new QRegExpValidator(QRegExp("0[2-4].*")));
+    layout->addWidget(edit_order, 4, 1);
+    edit_private = new QLineEdit();
+    if (is_for_point[5])
+        edit_private->setValidator(new QRegExpValidator(QRegExp("0[2-4].*")));
+    layout->addWidget(edit_private, 5, 1);
+    edit_public = new QLineEdit();
+    if (is_for_point[6])
+        edit_public->setValidator(new QRegExpValidator(QRegExp("0[2-4].*")));
+    edit_public->setReadOnly(true);
+    layout->addWidget(edit_public, 6, 1);
+    edit_other_public = new QLineEdit();
+    if (is_for_point[7])
+        edit_other_public->setValidator(new QRegExpValidator(QRegExp("0[2-4].*")));
+    layout->addWidget(edit_other_public, 7, 1);
+    edit_shared = new QLineEdit();
+    if (is_for_point[8])
+        edit_shared->setValidator(new QRegExpValidator(QRegExp("0[2-4].*")));
+    edit_shared->setReadOnly(true);
+    layout->addWidget(edit_shared, 8, 1);
 
-    resultHolder = new ResultHolder();
-    layout->addWidget(resultHolder, 2, 0);
+    edit_clear_text = new QTextEdit();
+    edit_clear_text->setPlaceholderText("Insert message you want to encode here");
+    layout->addWidget(edit_clear_text, 9, 1);
+    edit_cypher = new QTextEdit();
+    edit_cypher->setPlaceholderText("Insert message you want to decode here");
+    layout->addWidget(edit_cypher, 10, 1);
+    edit_base64 = new QTextEdit();
+    edit_base64->setPlaceholderText("Or insert encrypted message in base64 here");
+    layout->addWidget(edit_base64, 11, 1);
+    edit_sign = new QTextEdit();
+    edit_sign->setPlaceholderText("Field for signing message");
+    layout->addWidget(edit_sign, 12, 1);
+
+    button_load_curve = new QPushButton("Load curve from file");
+    layout->addWidget(button_load_curve, 6, 2);
+    button_load_key_pair = new QPushButton("Load private key from file");
+    layout->addWidget(button_load_key_pair, 7, 2);
+    button_load_other = new QPushButton("Load other key from file");
+    layout->addWidget(button_load_other, 8, 2);
+    button_save_curve = new QPushButton("Save curve to file");
+    layout->addWidget(button_save_curve, 9, 2);
+    button_save_key_pair = new QPushButton("Save key pair to file");
+    layout->addWidget(button_save_key_pair, 10, 2);
+    button_encode = new QPushButton("Encode");
+    layout->addWidget(button_encode, 11, 2);
+    button_decode = new QPushButton("Decode");
+    layout->addWidget(button_decode, 12, 2);
+
+    group_curve = new QGroupBox();
+    layout->addWidget(group_curve, 0, 2, 3, 1);
+
+    group_len = new QGroupBox();
+    layout->addWidget(group_len, 3, 2, 3, 1);
 
     setLayout(layout);
+    setMinimumWidth(1300);
+    set_len(256);
+}
 
-    connect(curveWidget, &CurveWidget::encodingFinishedSignal, resultHolder, &ResultHolder::encodingFinishedSlot);
-    connect(curveWidget, &CurveWidget::decodingFinishedSignal, resultHolder, &ResultHolder::decodingFinishedSlot);
-    connect(resultHolder, &ResultHolder::encodeSignal, curveWidget, &CurveWidget::encodeSlot);
-    connect(resultHolder, &ResultHolder::decodeSignal, curveWidget, &CurveWidget::decodeSlot);
+void MainWindow::set_len(int new_len) {
+    QString input_mask;
+    for (int i = 0; i < new_len + 15; i += 16)
+        input_mask += "HHHH ";
+    input_mask += ";0";
+    auto point_prefix = "\\09 ";
+    auto int_prefix = "   ";
+    edit_a->setInputMask(QString(">") + (is_for_point[0] ? point_prefix : int_prefix) + input_mask);
+    edit_b->setInputMask(QString(">") + (is_for_point[1] ? point_prefix : int_prefix) + input_mask);
+    edit_p->setInputMask(QString(">") + (is_for_point[2] ? point_prefix : int_prefix) + input_mask);
+    edit_base_point->setInputMask(QString(">") + (is_for_point[3] ? point_prefix : int_prefix) + input_mask);
+    edit_order->setInputMask(QString(">") + (is_for_point[4] ? point_prefix : int_prefix) + input_mask);
+    edit_private->setInputMask(QString(">") + (is_for_point[5] ? point_prefix : int_prefix) + input_mask);
+    edit_public->setInputMask(QString(">") + (is_for_point[6] ? point_prefix : int_prefix) + input_mask);
+    edit_other_public->setInputMask(QString(">") + (is_for_point[7] ? point_prefix : int_prefix) + input_mask);
+    edit_shared->setInputMask(QString(">") + (is_for_point[8] ? point_prefix : int_prefix) + input_mask);
+}
 
-    connect(curveWidget, &CurveWidget::keypairGeneratedSignal, keysHolder, &KeysHolder::keypairGenerated);
-    connect(curveWidget, &CurveWidget::sharedGeneratedSignal, keysHolder, &KeysHolder::sharedGenerated);
-    connect(curveWidget, &CurveWidget::publicByPrivateGeneratedSignal, keysHolder, &KeysHolder::publicByPrivateGenerated);
-    connect(curveWidget, &CurveWidget::requestKeysSignal, keysHolder, &KeysHolder::emitKeys);
-    connect(curveWidget, &CurveWidget::changeIntLenSignal, keysHolder, &KeysHolder::changeIntLen);
 
-    connect(keysHolder, &KeysHolder::keys, curveWidget, &CurveWidget::receiveKeysSlot);
-    connect(keysHolder, &KeysHolder::requestKeyGenerating, curveWidget, &CurveWidget::requestKeyGeneratingSlot);
-    connect(keysHolder, &KeysHolder::requestPublicByPrivate, curveWidget, &CurveWidget::requestPublicByPrivateSlot);
-    connect(keysHolder, &KeysHolder::requestSharedSecretGenerate, curveWidget, &CurveWidget::requestSharedSecretGenerate);
+void MainWindow::buttonLoadCurveSlot() {
+
+}
+
+void MainWindow::buttonLoadKeyPairSlot() {
+
+}
+
+void MainWindow::buttonLoadOtherSlot() {
+
+}
+
+void MainWindow::buttonSaveCurveSlot() {
+
+}
+
+void MainWindow::buttonSaveKeyPairSlot() {
+
+}
+
+void MainWindow::buttonEncodeSlot() {
+
+}
+
+void MainWindow::buttonDecodeSlot() {
+
+}
+
+void MainWindow::editCypherChangedSlot() {
+
+}
+
+void MainWindow::editBase64ChangedSlot() {
+
+}
+
+void MainWindow::groupCurvePressed() {
+
+}
+
+void MainWindow::groupLenPressed() {
+
+}
+
+void MainWindow::editCurveChanged() {
+
+}
+
+void MainWindow::editPrivateChanged() {
+
+}
+
+void MainWindow::editOtherChanged() {
+
 }
--- gui/MainWindow.h
+++ gui/MainWindow.h
@@ -7,22 +7,103 @@
 
 #include <QWidget>
 #include <QGridLayout>
-#include "CurveWidget.h"
-#include "KeysHolder.h"
-#include "ResultHolder.h"
+#include <QLabel>
+#include <QLineEdit>
+#include <QTextEdit>
+#include <QPushButton>
+#include <QGroupBox>
+#include <QString>
+#include <QVector>
+
+#include "../curve/ECC.h"
+#include <bitset>
 
 class MainWindow : public QWidget {
 Q_OBJECT
 
 private:
-    CurveWidget *curveWidget;
-    KeysHolder *keysHolder;
-    ResultHolder *resultHolder;
     QGridLayout *layout;
+
+    QLabel *label_a;
+    QLabel *label_b;
+    QLabel *label_p;
+    QLabel *label_base_point;
+    QLabel *label_order;
+    QLabel *label_private;
+    QLabel *label_public;
+    QLabel *label_other_public;
+    QLabel *label_shared;
+    QLabel *label_clear_text;
+    QLabel *label_cypher;
+    QLabel *label_base64;
+    QLabel *label_sign;
+
+    QLineEdit *edit_a;
+    QLineEdit *edit_b;
+    QLineEdit *edit_p;
+    QLineEdit *edit_base_point;
+    QLineEdit *edit_order;
+    QLineEdit *edit_private;
+    QLineEdit *edit_public;
+    QLineEdit *edit_other_public;
+    QLineEdit *edit_shared;
+
+    QTextEdit *edit_clear_text;
+    QTextEdit *edit_cypher;
+    QTextEdit *edit_base64;
+    QTextEdit *edit_sign;
+
+    QPushButton *button_load_curve;
+    QPushButton *button_load_key_pair;
+    QPushButton *button_load_other;
+    QPushButton *button_save_curve;
+    QPushButton *button_save_key_pair;
+    QPushButton *button_encode;
+    QPushButton *button_decode;
+
+    QGroupBox *group_curve, *group_len;
+    QVector<QPushButton *> curve_options;
+    QVector<QPushButton *> len_options;
+
+    ECC ecc;
+
+    std::bitset<9> is_for_point = {0b000100110};
+
+    void set_len(int new_len);
 
 public:
 
-    MainWindow();
+    explicit MainWindow(QWidget * = Q_NULLPTR);
+
+private slots:
+
+    void buttonLoadCurveSlot();
+
+    void buttonLoadKeyPairSlot();
+
+    void buttonLoadOtherSlot();
+
+    void buttonSaveCurveSlot();
+
+    void buttonSaveKeyPairSlot();
+
+    void buttonEncodeSlot();
+
+    void buttonDecodeSlot();
+
+    void editCypherChangedSlot();
+
+    void editBase64ChangedSlot();
+
+    void groupCurvePressed();
+
+    void groupLenPressed();
+
+    void editCurveChanged();
+
+    void editPrivateChanged();
+
+    void editOtherChanged();
 
 };
 
